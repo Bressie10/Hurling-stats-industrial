1bead8104aa69e5f1527e26005c390f41e9f5f9b
Ultan Breslin <ultanbreslin@Ultans-MacBook-Neo.local>
2026-04-10 17:58:23 +0100

    Move Supabase credentials to env variables

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
-const SUPABASE_URL = 'https://syikhsgovqogzkmmhuis.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5aWtoc2dvdnFvZ3prbW1odWlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyOTk0MjEsImV4cCI6MjA5MDg3NTQyMX0.wL5b1Udyv8nSPqwX-Au9FejwTGY0X2gdAcMmMFrD7LM'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
